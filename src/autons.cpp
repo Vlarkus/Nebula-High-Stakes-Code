3258f29327062f916c52198cc17a191a28295274
Vlarkus <169931981+Vlarkus@users.noreply.github.com>
2025-03-07 18:29:52 -0500

    Not working Skills

--- src/autons.cpp
+++ src/autons.cpp
@@ -83,168 +83,58 @@ Routine routines[] = {
 
         // Setup
         LADYBROWN::initialize();
-        chassis.setPose(-58.457, -0.249, 270);
+        chassis.setPose(0, 0, 180);
 
-        // Score alliance stake
         LADYBROWN::setTargetPosition(240);
         delay(1500);
-
-        chassis.follow(autonSkills["Path 1"], 7, 1000, false);
-        chassis.waitUntilDone();
-        LADYBROWN::setTargetPosition(3);
-
         
-        chassis.turnToHeading(0,1000);
-        chassis.follow(autonSkills["Path 2"], 7, 1000, false);
+        chassis.moveToPose(0, 10, 90, 1000, BACKWARDS);
         chassis.waitUntilDone();
-
-        MOGO::close();
-        INTAKE::in();
-        chassis.turnToHeading(90,1000);
-        chassis.follow(autonSkills["Path 3"], 7, 1000);
-        chassis.waitUntilDone();
-
-        chassis.turnToHeading(132,1000);
-        chassis.follow(autonSkills["Path 4"], 7, 1000);
-        chassis.waitUntil(12);
-        LADYBROWN::setTargetPosition(35);
-        chassis.waitUntilDone();
-
-        chassis.turnToHeading(277,1000);
-        INTAKE::stop();
-        chassis.follow(autonSkills["Path 5"], 7, 1000);
-        chassis.waitUntilDone();
-
-        LADYBROWN::setTargetPosition(80);
-        INTAKE::in();
-        chassis.turnToHeading(180,1000);
-        chassis.follow(autonSkills["Path 6"], 7, 1000);
-        chassis.waitUntilDone();
-
-        LADYBROWN::setTargetPosition(140);
-        chassis.follow(autonSkills["Path 7"], 7, 1000, false);
-        chassis.waitUntilDone();
-
+        
+        chassis.moveToPose(-24, 10, 90, 1000, BACKWARDS);
         LADYBROWN::setTargetPosition(3);
-        chassis.turnToHeading(265,1000);
-        chassis.follow(autonSkills["Path 8"], 7, 1000);
         chassis.waitUntilDone();
-
-        chassis.turnToHeading(271,1000);
-        chassis.follow(autonSkills["Path 9"], 7, 1000);
-        chassis.waitUntilDone();
-
-        chassis.turnToHeading(268,1000);
-        chassis.follow(autonSkills["Path 10"], 7, 1000);
-        chassis.waitUntilDone();
-
-        chassis.turnToHeading(150,1000);
-        chassis.follow(autonSkills["Path 11"], 7, 1000);
-        chassis.waitUntilDone();
-
-        chassis.turnToHeading(80,1000);
-        MOGO::open();
-        chassis.follow(autonSkills["Path 12"], 7, 1000,false);
-        chassis.waitUntilDone();
-
-        chassis.turnToHeading(82,1000);
-        LADYBROWN::setTargetPosition(35);
-        chassis.follow(autonSkills["Path 13"], 7, 1000);
-        chassis.waitUntilDone();
-
         delay(500);
-        INTAKE::stop();
-        chassis.turnToHeading(307,1000);
-        chassis.follow(autonSkills["Path 14"], 7, 1000);
-        chassis.waitUntilDone();
-
-        chassis.turnToHeading(166,1000);
-        chassis.follow(autonSkills["Path 15"], 7, 1000);
-        chassis.waitUntilDone();
-
-        chassis.follow(autonSkills["Path 16"], 7, 1000, false);
-        chassis.waitUntilDone();
-
+        
         MOGO::close();
-        chassis.turnToHeading(90,1000);
-        chassis.follow(autonSkills["Path 17"], 7, 1000);
-        chassis.waitUntilDone();
+        delay(1000);
+        
 
-        LADYBROWN::setTargetPosition(240);
-        chassis.follow(autonSkills["Path 18"], 7, 1000, false);
-        chassis.waitUntilDone();
 
-        LADYBROWN::setTargetPosition(3);
+        chassis.turnToPoint(-30, 32, 1000);
+        chassis.waitUntilDone();
         INTAKE::in();
-        chassis.turnToHeading(225,1000);
-        chassis.follow(autonSkills["Path 19"], 7, 1000);
-        chassis.waitUntilDone();
 
-        chassis.turnToHeading(315,1000);
-        INTAKE::stop();
-        chassis.follow(autonSkills["Path 20"], 7, 1000);
+        chassis.moveToPose(-30, 32, 90, 1000, FORWARDS);
         chassis.waitUntilDone();
+        delay(1000);
 
-        chassis.follow(autonSkills["Path 21"], 7, 1000);
-        INTAKE::in();
-        chassis.waitUntilDone();
 
-        chassis.turnToHeading(268,1000);
-        chassis.follow(autonSkills["Path 22"], 7, 1000);
-        chassis.waitUntilDone();
-
-        chassis.turnToHeading(31,1000);
-        chassis.follow(autonSkills["Path 23"], 7, 1000);
-        chassis.waitUntilDone();
-
-        chassis.turnToHeading(270,1000);
-        chassis.follow(autonSkills["Path 24"], 7, 1000);
+        chassis.turnToPoint(-48, 32, 1000);
         chassis.waitUntilDone();
         
-        chassis.turnToHeading(112,1000);
-        MOGO::open();
-        chassis.follow(autonSkills["Path 25"], 7, 1000);
+        chassis.moveToPose(-48, 32, 90, 1000, FORWARDS);
+        chassis.waitUntilDone();
+        delay(1000);
+
+
+
+        chassis.turnToPoint(-60, 54, 1000);
         chassis.waitUntilDone();
 
-        MOGO::close();
-        chassis.turnToHeading(45,1000);
-        chassis.follow(autonSkills["Path 26"], 7, 1000);
+        chassis.moveToPose(-60, 52, 90, 1000, FORWARDS);
+        chassis.waitUntilDone();
+        delay(1000);
+
+
+
+        chassis.turnToPoint(-60, 52, 1000);
         chassis.waitUntilDone();
 
-        chassis.turnToHeading(66,1000);
-        chassis.follow(autonSkills["Path 27"], 7, 1000);
+        chassis.moveToPose(-60, 52, 90, 1000, FORWARDS);
         chassis.waitUntilDone();
-        chassis.turnToHeading(0,1000);
-        chassis.follow(autonSkills["Path 28"], 7, 1000);
-        chassis.waitUntilDone();
-        chassis.follow(autonSkills["Path 29"], 7, 1000, false);
-        chassis.waitUntilDone();
-        chassis.turnToHeading(102,1000);
-        chassis.follow(autonSkills["Path 30"], 7, 1000);
-        chassis.waitUntilDone();
-        chassis.turnToHeading(180,1000);
-        chassis.follow(autonSkills["Path 31"], 7, 1000);
-        chassis.waitUntilDone();
-        chassis.turnToHeading(45,1000);
-        chassis.follow(autonSkills["Path 32"], 7, 1000);
-        chassis.waitUntilDone();
-        chassis.turnToHeading(90,1000);
-        chassis.follow(autonSkills["Path 33"], 7, 1000);
-        chassis.waitUntilDone();
-        chassis.turnToHeading(326,1000);
-        chassis.follow(autonSkills["Path 34"], 7, 1000);
-        chassis.waitUntilDone();
-        chassis.turnToHeading(77,1000);
-        chassis.follow(autonSkills["Path 35"], 7, 1000, false);
-        chassis.waitUntilDone();
-        chassis.follow(autonSkills["Path 36"], 7, 1000);
-        chassis.waitUntilDone();
-        chassis.turnToHeading(27,1000);
-        chassis.follow(autonSkills["Path 37"], 7, 1000, false);
-        chassis.waitUntilDone();
-        chassis.turnToHeading(185,1000);
-        chassis.follow(autonSkills["Path 38"], 7, 1000, false);
-        chassis.waitUntilDone();
+        delay(1000);
+        
     })
 
 };
