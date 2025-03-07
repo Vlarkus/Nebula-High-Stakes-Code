071f52d3a8e6cbb950fe624dc2215b4a3a07c044
FuzzDOT <fuzzinc45@gmail.com>
2025-03-07 12:32:10 -0500

    thingymabob

--- src/autons.cpp
+++ src/autons.cpp
@@ -27,6 +27,9 @@ using namespace pros;
  * │ ROUTINES │
  * ╰──────────╯
  */
+ASSET(autonSkillsRoute_txt);
+lemlib_tarball::Decoder autonSkills(autonSkillsRoute_txt);
+
 
 #define FORWARDS {.forwards = true}
 #define BACKWARDS {.forwards = false}
@@ -62,9 +65,136 @@ Routine routines[] = {
 
     }),
 
-    Routine("Driver Skills", "Driver Skills Route", []() {
-        chassis.setPose(-57.934, -0.236, 270);
-        LADYBROWN::control();
+    Routine("Auton Skills", "Auton Skills Route", []() {
+        chassis.setPose(-58.457, -0.249, 270);
+        //ladybrown alliance stake score
+        chassis.follow(autonSkills["Path 1"], 7, 1000, false);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(180,1000);
+        chassis.follow(autonSkills["Path 2"], 7, 1000);
+        chassis.waitUntilDone();
+        MOGO::close();
+        INTAKE::in();
+        chassis.turnToHeading(90,1000);
+        chassis.follow(autonSkills["Path 3"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(132,1000);
+        chassis.follow(autonSkills["Path 4"], 7, 1000);
+        //ladybrown load
+        chassis.waitUntilDone();
+        chassis.turnToHeading(277,1000);
+        INTAKE::stop();
+        chassis.follow(autonSkills["Path 5"], 7, 1000);
+        chassis.waitUntilDone();
+        //ladybrown preload
+        INTAKE::in();
+        chassis.turnToHeading(180,1000);
+        chassis.follow(autonSkills["Path 6"], 7, 1000);
+        chassis.waitUntilDone();
+        //ladybrown score
+        chassis.follow(autonSkills["Path 7"], 7, 1000, false);
+        chassis.waitUntilDone();
+        //ladybrown store
+        chassis.turnToHeading(265,1000);
+        chassis.follow(autonSkills["Path 8"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(271,1000);
+        chassis.follow(autonSkills["Path 9"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(268,1000);
+        chassis.follow(autonSkills["Path 10"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(150,1000);
+        chassis.follow(autonSkills["Path 11"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(80,1000);
+        MOGO::open();
+        chassis.follow(autonSkills["Path 12"], 7, 1000,false);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(82,1000);
+        //ladybrown load
+        chassis.follow(autonSkills["Path 13"], 7, 1000);
+        chassis.waitUntilDone();
+        delay(500);
+        INTAKE::stop();
+        chassis.turnToHeading(307,1000);
+        chassis.follow(autonSkills["Path 14"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(166,1000);
+        chassis.follow(autonSkills["Path 15"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.follow(autonSkills["Path 16"], 7, 1000, false);
+        chassis.waitUntilDone();
+        MOGO::close();
+        chassis.turnToHeading(90,1000);
+        chassis.follow(autonSkills["Path 17"], 7, 1000);
+        chassis.waitUntilDone();
+        //ladybrown alliance stake score
+        chassis.follow(autonSkills["Path 18"], 7, 1000, false);
+        chassis.waitUntilDone();
+        //ladybrown store
+        INTAKE::in();
+        chassis.turnToHeading(225,1000);
+        chassis.follow(autonSkills["Path 19"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(315,1000);
+        INTAKE::stop();
+        chassis.follow(autonSkills["Path 20"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.follow(autonSkills["Path 21"], 7, 1000);
+        INTAKE::in();
+        chassis.waitUntilDone();
+        chassis.turnToHeading(268,1000);
+        chassis.follow(autonSkills["Path 22"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(31,1000);
+        chassis.follow(autonSkills["Path 23"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(270,1000);
+        chassis.follow(autonSkills["Path 24"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(112,1000);
+        MOGO::open();
+        chassis.follow(autonSkills["Path 25"], 7, 1000);
+        chassis.waitUntilDone();
+        MOGO::close();
+        chassis.turnToHeading(45,1000);
+        chassis.follow(autonSkills["Path 26"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(66,1000);
+        chassis.follow(autonSkills["Path 27"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(0,1000);
+        chassis.follow(autonSkills["Path 28"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.follow(autonSkills["Path 29"], 7, 1000, false);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(102,1000);
+        chassis.follow(autonSkills["Path 30"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(180,1000);
+        chassis.follow(autonSkills["Path 31"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(45,1000);
+        chassis.follow(autonSkills["Path 32"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(90,1000);
+        chassis.follow(autonSkills["Path 33"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(326,1000);
+        chassis.follow(autonSkills["Path 34"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(77,1000);
+        chassis.follow(autonSkills["Path 35"], 7, 1000, false);
+        chassis.waitUntilDone();
+        chassis.follow(autonSkills["Path 36"], 7, 1000);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(27,1000);
+        chassis.follow(autonSkills["Path 37"], 7, 1000, false);
+        chassis.waitUntilDone();
+        chassis.turnToHeading(185,1000);
+        chassis.follow(autonSkills["Path 38"], 7, 1000, false);
+        chassis.waitUntilDone();
     })
 
 };
